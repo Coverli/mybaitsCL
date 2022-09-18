56e8266ca88ca645a2dfa27f5d51c75bf957dd4a
Coverli <2870118626@qq.com>
2022-09-19 01:16:17 +0800

    bat批处理自动推送:2022/09/19, 1:16:17

--- day04/src/test/java/com/lazy/AccountTest.java
+++ day04/src/test/java/com/lazy/AccountTest.java
@@ -1,7 +1,7 @@
-package com.manyTOmany;
+package com.lazy;
 
-import com.manyTOmany.dao.IRoleDao;
-import com.manyTOmany.domain.Role;
+import com.lazy.dao.IAccountDao;
+import com.lazy.domain.Account;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -13,22 +13,22 @@ import org.junit.Test;
 import java.io.InputStream;
 import java.util.List;
 
-public class RoleTest {
+public class AccountTest {
 
     private InputStream in;
     private SqlSession sqlSession;
-    private IRoleDao roleDao;
+    private IAccountDao accountDao;
 
     @Before//用于在测试方法执行之前执行
     public void init()throws Exception{
         //1.读取配置文件，生成字节输入流
-        in = Resources.getResourceAsStream("SqlMapConfigmanyTOmany.xml");
+        in = Resources.getResourceAsStream("SqlMapConfiglazy.xml");
         //2.获取SqlSessionFactory
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
         //3.获取SqlSession对象
         sqlSession = factory.openSession(true);
         //4.获取dao的代理对象
-        roleDao = sqlSession.getMapper(IRoleDao.class);
+        accountDao = sqlSession.getMapper(IAccountDao.class);
     }
 
     @After//用于在测试方法执行之后执行
@@ -45,11 +45,11 @@ public class RoleTest {
      */
     @Test
     public void testFindAll(){
-        List<Role> roles = roleDao.findAll();
-        for(Role role : roles){
-            System.out.println("---每个角色的信息----");
-            System.out.println(role);
-            System.out.println(role.getUsers());
-        }
+        List<Account> accounts = accountDao.findAll();
+//        for(Account account : accounts){
+//            System.out.println("--------每个account的信息------------");
+//            System.out.println(account);
+//            System.out.println(account.getUser());
+//        }
     }
 }
--- day04/src/test/java/com/lazy/UserTest.java
+++ day04/src/test/java/com/lazy/UserTest.java
@@ -1,7 +1,7 @@
-package com.manyTOmany;
+package com.lazy;
 
-import com.manyTOmany.dao.IRoleDao;
-import com.manyTOmany.domain.Role;
+import com.lazy.dao.IUserDao;
+import com.lazy.domain.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -13,22 +13,22 @@ import org.junit.Test;
 import java.io.InputStream;
 import java.util.List;
 
-public class RoleTest {
+public class UserTest {
 
     private InputStream in;
     private SqlSession sqlSession;
-    private IRoleDao roleDao;
+    private IUserDao userDao;
 
     @Before//用于在测试方法执行之前执行
     public void init()throws Exception{
         //1.读取配置文件，生成字节输入流
-        in = Resources.getResourceAsStream("SqlMapConfigmanyTOmany.xml");
+        in = Resources.getResourceAsStream("SqlMapConfiglazy.xml");
         //2.获取SqlSessionFactory
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
         //3.获取SqlSession对象
         sqlSession = factory.openSession(true);
         //4.获取dao的代理对象
-        roleDao = sqlSession.getMapper(IRoleDao.class);
+        userDao = sqlSession.getMapper(IUserDao.class);
     }
 
     @After//用于在测试方法执行之后执行
@@ -45,11 +45,11 @@ public class RoleTest {
      */
     @Test
     public void testFindAll(){
-        List<Role> roles = roleDao.findAll();
-        for(Role role : roles){
-            System.out.println("---每个角色的信息----");
-            System.out.println(role);
-            System.out.println(role.getUsers());
-        }
+        List<User> users = userDao.findAll();
+//        for(User user : users){
+//            System.out.println("-----每个用户的信息------");
+//            System.out.println(user);
+//            System.out.println(user.getAccounts());
+//        }
     }
 }
