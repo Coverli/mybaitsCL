0b56279cbf21a4f40d95d49ae423491897975b57
Coverli <2870118626@qq.com>
2022-09-18 17:49:17 +0800

    bat批处理自动推送:2022/09/18,17:49:16

---
+++ day03/src/main/java/com/JNDI/domain/QueryVo.java
@@ -0,0 +1,14 @@
+package com.JNDI.domain;
+
+public class QueryVo {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
